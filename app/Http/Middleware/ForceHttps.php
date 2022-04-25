c444937b5f90bce5962596829ce341755dd8ec0b
hash0102 <s19161485@st.daito.ac.jp>
2022-04-25 21:32:49 +0900

    HerokuのHttps化練習4

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -17,19 +17,19 @@ class ForceHttps
     
     public function handle($request, Closure $next)
     {
-    //     if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
-    //     return redirect()->secure($request->getRequestUri());
-    // }
-    //     return $next($request);
-    // }
-    
-     if (env('APP_ENV') === 'production') {
-            $request->server->set('HTTPS', 'on');
-            if ($request->header('x-forwarded-proto') <> 'https') {
-                return redirect()->secure($request->getRequestUri());
-            }
-        }
-
+        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+        return redirect()->secure($request->getRequestUri());
+    }
         return $next($request);
     }
+    
+    //  if (env('APP_ENV') === 'production') {
+    //         $request->server->set('HTTPS', 'on');
+    //         if ($request->header('x-forwarded-proto') <> 'https') {
+    //             return redirect()->secure($request->getRequestUri());
+    //         }
+    //     }
+
+      //  return $next($request);
+   // }
 }
